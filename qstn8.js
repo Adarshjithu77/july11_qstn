4b80406f7f0c3cf81aab38217161631de88228fb
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-14 03:25:23 +0530

    eighth qstn

---
+++ qstn8.js
@@ -0,0 +1,6 @@
+const allps=document.getElementsByTagName("p");
+for(let i=0;i<allps.length;i++){
+    allps[i].style.border="1px solid black";
+    allps[i].style.backgroundColor="blue";
+    allps[i].style.padding="10px";
+}
